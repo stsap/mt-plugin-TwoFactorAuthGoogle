94d17464d508416efca177182a28d21367315cbe
root <root@dev.onagatani.com>
2015-02-02 12:13:08 +0900

    修正

--- plugins/TwoFactorAuthGoogle/TwoFactorAuthGoogle.pl
+++ plugins/TwoFactorAuthGoogle/TwoFactorAuthGoogle.pl
@@ -127,8 +127,8 @@ sub _tmpl_source_login {
     my ($cb, $app, $tmpl) = @_;
     my $plugin = $app->component("TwoFactorAuthGoogle");
     my $config = {};
-    $plugin->load_config($config, 'system');
-    return if (not $config->{"enable_two_factor_auth"});
+#    $plugin->load_config($config, 'system');
+#    return if (not $config->{"enable_two_factor_auth"});
 
     my $add = $plugin->translate_templatized(<<'HTML');
 <mtapp:setting
